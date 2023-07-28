c1ba5a1c497291f5124c2c7d1e5ec21b3f3dd7f7
nwabimady <nwabimady@gmail.com>
2023-07-28 21:35:24 +0200

    JSX: Hello World_Google

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
+
+function getGreetElement(name, link) {
+    const message = `Hello, ${name}`;
+    return (<div> 
+                <h1>{message}</h1>
+                <a href={link}>link</a>
+            </div>);
+}
+
+const block = getGreetElement("Nwabisa", "https://www.google.com/");
+
+root.render(block);
