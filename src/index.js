31790cc83eccc84f178f19bc776e29b2692cdb8e
nwabimady <nwabimady@gmail.com>
2023-07-28 21:43:43 +0200

    React Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import ReactDOM from "react-dom";
+import { HelloWorld } from "./components/hello world";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function getGreetElement(name, link) {
-    const message = `Hello, ${name}`;
-    return (<div> 
-                <h1>{message}</h1>
-                <a href={link}>link</a>
-            </div>);
-}
-
-const block = getGreetElement("Nwabisa", "https://www.google.com/");
-
-root.render(block);
+root.render(<HelloWorld/>);
