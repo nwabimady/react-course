8eaae4d2a04168fdde3993a706b32febbff029fa
nwabimady <nwabimady@gmail.com>
2023-07-28 21:53:10 +0200

    Strictmode

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import { HelloWorld } from "./components/hello world";
+import { App } from "./App";
+import { StrictMode } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HelloWorld/>);
-=======
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function getGreetElement(name, link) {
-    const message = `Hello, ${name}`;
-    return (<div> 
-                <h1>{message}</h1>
-                <a href={link}>link</a>
-            </div>);
-}
-
-const block = getGreetElement("Nwabisa", "https://www.google.com/");
-
-root.render(block);
->>>>>>> main
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
