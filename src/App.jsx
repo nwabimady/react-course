3d61c5ca1e2ebe1699ec317da024ff727392c4c8
nwabimady <nwabimady@gmail.com>
2023-08-03 00:32:14 +0200

    Conditional Rendering: If/Else

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./components/global.css";
 import { HelloCounter } from "./components/hello-counter";
+import { HelloCheckbox } from "./components/hello-checkbox";
 
 export function App() {
     return (
       <>
+        <HelloCheckbox/>
         <HelloCounter/>
       </>
     );
--- src/components/hello-checkbox.jsx
+++ src/components/hello-checkbox.jsx
@@ -1,9 +1,23 @@
-export const HelloCheckbox = ({ person }) => {
-    const clickHandler = (event) => {
-        if(event.target.checked) {
-        alert(`Hello ${person.name}`);
-        }
-    };
-    return <input type="checkbox" onChange={clickHandler} />;
-};
+import { useState } from "react";
 
+export const HelloCheckbox = () => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const onChangeHandler = (event) => {
+        setIsChecked(event.target.checked);
+    }
+    
+    const getConditionalContent = () => {
+        if (isChecked) {
+            return <p>This is checked!</p>;
+        }
+        return <p>Nope!</p>
+    };
+
+return (
+    <>
+     <input onChange= {onChangeHandler} type="checkbox" />
+     {getConditionalContent()}
+    </>
+);
+};
