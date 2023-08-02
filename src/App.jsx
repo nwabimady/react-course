551f60b545b74bc52371c07e3ab16e7e0ef85c49
nwabimady <nwabimady@gmail.com>
2023-08-02 23:07:02 +0200

    React.fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import React from "react";
 import { HelloCheckbox } from "./components/hello-checkbox";
+import { HelloButton } from "./components/hello-button";
 
 export function App() {
   const person = { name: "Nwabisa", surname: "Redwood-Sawyerr" };
-    return <HelloCheckbox person={person} />;
+    return (
+      <React.Fragment>
+        <HelloCheckbox person={person} />
+        <HelloButton person={person} />
+      </React.Fragment>
+    );
 }
