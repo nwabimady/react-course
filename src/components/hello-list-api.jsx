474ef6d1a5322f89e1455d93b11425f955640570
nwabimady <nwabimady@gmail.com>
2023-08-03 21:47:10 +0200

    API:Async

--- src/components/hello-list-api.jsx
+++ src/components/hello-list-api.jsx
@@ -4,17 +4,32 @@ export const HelloListApi = () => {
   const [names, setNames] = useState([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=4").then((response) => {
+    //     fetch("https://randomuser.me/api/?results=4").then((response) => {
+    //       if (response) {
+    //         response.json().then((data) => {
+    //           const newNames = [];
+    //           data.results.forEach((result) => {
+    //             newNames.push(result.name.first);
+    //           });
+    //           setNames(newNames);
+    //         });
+    //       }
+    //     });
+    //   }, []);
+
+    const fetchNames = async () => {
+      const response = await fetch("https://randomuser.me/api/?results=4");
       if (response) {
-        response.json().then((data) => {
-          const newNames = [];
-          data.results.forEach((result) => {
-            newNames.push(result.name.first);
-          });
-          setNames(newNames);
-        });
+        const data = await response.json();
+        const newNames = [];
+        for (const result of data.result) {
+          newNames.push(result.name.first);
+        }
+        setNames(newNames);
       }
-    });
+    };
+
+    fetchNames();
   }, []);
 
   return (
