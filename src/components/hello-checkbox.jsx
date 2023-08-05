afbacf44e01ac4bfee167a963d4c02887f91f9ed
nwabimady <nwabimady@gmail.com>
2023-08-06 01:26:44 +0200

    Implementing Advanced Context with State Hook

--- src/components/hello-checkbox.jsx
+++ src/components/hello-checkbox.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { MyContext } from "./hello-context";
 
 export const HelloCheckbox = () => {
-    const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useContext(MyContext);
 
-    const onChangeHandler = (event) => {
-        setIsChecked(event.target.checked);
-    };
+  const onChangeHandler = (event) => {
+    setIsChecked(event.target.checked);
+  };
 
-return (
+  return (
     <>
-     <input onChange= {onChangeHandler} type="checkbox" />
-     {isChecked ? <p>This is checked!</p> : <p>Nope!</p>}
+      <input onChange={onChangeHandler} type="checkbox" />
+      {isChecked ? <p>This is checked!</p> : <p>Nope!</p>}
     </>
-);
+  );
 };
--- src/components/hello-form.jsx
+++ src/components/hello-form.jsx
@@ -1,46 +1,44 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { MyContext } from "./hello-context";
 
 export const HelloForm = () => {
-    
-    const [name, setName] = useState (" ");
-    const [surname, setSurname] = useState (" ");
-    
-    const clickHandler = (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        alert(`Hello ${name} ${surname}!`);
-        setName("");
-        setSurname(""); 
-    }
+  const [name, setName] = useState(" ");
+  const [surname, setSurname] = useState(" ");
+  const [isChecked] = useContext(MyContext);
 
-    return (
-        <form>
-            <label>
-                Name: 
-                <input 
-                    value={name}
-                    onChange={
-                        (event) => {
-                            setName(event.target.value)
-                    }
-                }>
-                    
-                </input>
-            </label>
-            <label>
-                Surname: <input 
-                    value={surname}
-                    onChange={
-                        (event) => {
-                            setSurname(event.target.value)
-                    }
-                }></input>
-            </label>
-            <label>
-                <button onClick={clickHandler}>
-                    Submit
-                </button>
-            </label>
-        </form>
-        );
+  const clickHandler = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    alert(`Hello ${name} ${surname}!`);
+    setName("");
+    setSurname("");
+  };
+
+  return (
+    <form>
+      <label>
+        Name:
+        <input
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+      </label>
+      <label>
+        Surname:{" "}
+        <input
+          value={surname}
+          onChange={(event) => {
+            setSurname(event.target.value);
+          }}
+        ></input>
+      </label>
+      <label>
+        <button disabled={isChecked} onClick={clickHandler}>
+          Submit
+        </button>
+      </label>
+    </form>
+  );
 };
