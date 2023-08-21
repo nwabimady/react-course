4594896e0616680c574e3bf0a922467acab0f44b
nwabimady <nwabimady@gmail.com>
2023-08-21 22:52:41 +0200

    React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import "./components/global.css";
-import { AppContext } from "./App-context";
-import { HelloCheckbox } from "./components/hello-checkbox";
-import { HelloForm } from "./components/hello-form";
-import { HelloCounter } from "./components/hello-counter";
+import { HelloListApi } from "./components/hello-list-api";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { HelloWorld } from "./components/hello-world";
 
 export function App() {
   return (
-    <>
-      <AppContext>
-        <HelloCheckbox />
-        <HelloForm />
-        <HelloCounter />
-      </AppContext>
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/List">List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/list" element={<HelloListApi />}></Route>
+          <Route
+            path="/"
+            element={
+              <HelloWorld
+                person={{ name: "Nwabisa", surname: "Redwood-Sawyerr" }}
+              />
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
